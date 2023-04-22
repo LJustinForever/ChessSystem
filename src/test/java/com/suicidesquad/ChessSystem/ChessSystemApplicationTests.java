41d73a1e1498730caf76ce95317a14f5567c229e
Lisajus <justas.lisajus@ktu.edu>
2023-04-22 22:13:49 +0300

    initial commit

---
+++ src/test/java/com/suicidesquad/ChessSystem/ChessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suicidesquad.ChessSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
